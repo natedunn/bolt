e70b2cea1b73883cea22af1f634042d4bfedee90
Nate Dunn <hello@natedunn.net>
2016-05-07 16:03:07 -0700

    Update to v2 beta gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,27 @@
 // DEPENDENCIES
 // ======================
 
-var gulp = require('gulp')
-  minifycss = require('gulp-minify-css'),
+var gulp = require('gulp'),
   sass = require('gulp-ruby-sass'),
   sourcemaps = require('gulp-sourcemaps'),
-  // autoprefixer = require('gulp-autoprefixer'),
   rename = require('gulp-rename'),
   concat = require('gulp-concat'),
   notify = require('gulp-notify'),
   watch = require('gulp-watch'),
-  csscomb = require('gulp-csscomb'),
   gutil = require('gulp-util'),
   plumber = require('gulp-plumber'),
   browserSync = require('browser-sync'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  cssnano = require('cssnano'),
-  // uncss = require('gulp-uncss')
-  merge = require('merge2'),
   nunjucksRender = require('gulp-nunjucks-render'),
   prettify = require('gulp-prettify'),
   runSequence = require('run-sequence').use(gulp),
   symdiff = require('gulp-symdiff'),
   symdiffhtml = require('symdiff-html'),
   symdiffcss = require('symdiff-css'),
+  fs = require('fs'),
   del = require('del'),
-  fs = require('fs');
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('cssnano');
 
 // ======================
 // AUTOMATION
@@ -48,7 +43,7 @@ var reload = browserSync.reload;
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-      baseDir: "./test/"
+      baseDir: "./test"
     }
   });
 });
@@ -56,88 +51,85 @@ gulp.task('browser-sync', function() {
 gulp.task('bs-reload', function() { browserSync.reload(); });
 
 gulp.task('watch', ['browser-sync'], function() {
-  // Template Files
-  gulp.watch('test/**/*.html', ['bs-reload']);
+  // HTML Files
+  gulp.watch('test/**/*.html');
   // Project Styles
-  gulp.watch('scss/**/*.scss', ['styles'])
+  gulp.watch('test/styles/**/*.scss', ['styles-dev', 'styles-pro'])
 });
 
 // ======================
 // STYLES
 // ======================
 
-// Clean Styles
-gulp.task('clean:styles', function () {
-  return del([
-    'test/styles/**/*'
-  ]);
-});
-
 // Dev Styles
-gulp.task('styles', ['clean:styles'], function() {
-  var processors = [
-    autoprefixer({browsers: ['last 2 version']}),
-    // cssnano
-  ];
-  return sass('./scss/bolt.scss', {})
+gulp.task('styles-dev', function() {
+  return sass('./src/styles/bolt.scss', {style:'nested'})
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(sourcemaps.init())
     .pipe(postcss([autoprefixer({browsers: ['last 2 version']})]))
+    .pipe(sourcemaps.init())
+    .pipe(postcss(plugins))
     .pipe(concat('main.dev.css'))
-    .pipe(gulp.dest('./test/styles'))
-    .pipe(postcss([cssnano]))
-    .pipe(rename('main.min.css'))
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./test/styles'))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ message: 'DEVELOPMENT STYLES task complete' }));
 });
 
+// Styles Production
+gulp.task('styles-prod', function() {
+  return sass('./src/styles/bolt.scss', { style: 'nested' })
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(postcss([autoprefixer({browsers: ['last 2 version']})]))
+    .pipe(postcss([cssnano({discardComments: {removeAll: true}})]))
+    .pipe(concat('main.min.css'))
+    .pipe(gulp.dest('./test/styles'))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(notify({ message: 'PRODUCTION STYLES task complete' }));
+});
+
 // IE Styles
 gulp.task('styles-ie', function() {
-  return sass('./scss/bolt-ie.scss', {})
+  return sass(['./src/styles/bolt-ie.scss'], { style: 'nested' })
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(autoprefixer(
+    .pipe(postcss([autoprefixer({browsers: [
       'last 2 version',
       'ie 7',
       'ie 8',
       'ie 9'
-    ))
-    .pipe(concat('ie.css'))
-    .pipe(gulp.dest('./test/styles'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
+    ]})]))
+    .pipe(concat('ie.main.css'))
     .pipe(gulp.dest('./test/styles'))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ message: 'IE STYLES task complete' }));
 });
 
 // ======================
-// CHECKS
+// IMAGES
 // ======================
 
-gulp.task('check', function() {
+gulp.task('images', ['clean:images'], function() {
   return gulp.src([
-    'test/styles/main.min.css',
-    'test/**/*.html'
+    './bower_components/final/images/**/*',
+    './src/images/**/*'
   ])
-    .pipe(symdiff({
-      // ignore: [],
-      templates: [symdiffhtml],
-      css: [symdiffcss]
-    })
-    .on('error', function() {
-        process.exit(0);
-    }))
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('./test/images'))
 });
 
 // ======================
-// TEST
+// CLEAN
 // ======================
 
-gulp.task('test', function(callback) {
-  runSequence(
-    'styles',
-    'styles-ie',
-    callback);
+// Clean Styles
+gulp.task('clean:styles', function () {
+  return del('test/styles/**/*');
+});
+
+// ======================
+// BUILD
+// ======================
+
+gulp.task('test', function(callback){
+  'clean:styles',
+  'styles-dev',
+  'styles-prod'
 });
