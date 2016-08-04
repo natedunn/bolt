daa8f77e6fb4de4b017e58f3c231c2b30dbdff87
Nate Dunn <hello@natedunn.net>
2016-08-04 14:56:17 -0700

    Update with new organization

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('watch', ['browser-sync'], function() {
 
 // Dev Styles
 gulp.task('styles-dev', function() {
-  return sass('styles/bolt-test.scss', {style:'nested'})
+  return sass('styles/manifest-test.scss', {style:'nested'})
     .pipe(plumber({ errorHandler: onError }))
     .pipe(postcss([
       autoprefixer({browsers: ['last 2 version']}),
@@ -84,7 +84,7 @@ gulp.task('styles-dev', function() {
 
 // Styles Production
 gulp.task('styles-prod', function() {
-  return sass('styles/bolt-test.scss', { style: 'nested' })
+  return sass('styles/manifest-test.scss', { style: 'nested' })
     .pipe(plumber({ errorHandler: onError }))
     .pipe(postcss([
       autoprefixer({browsers: ['last 2 version']}),
@@ -104,7 +104,7 @@ gulp.task('styles-prod', function() {
 
 // IE Styles
 gulp.task('styles-ie', function() {
-  return sass(['styles/bolt-ie.scss'], { style: 'nested' })
+  return sass(['styles/manifest-ie.scss'], { style: 'nested' })
     .pipe(plumber({ errorHandler: onError }))
     .pipe(postcss([autoprefixer({browsers: [
       'last 2 version',
